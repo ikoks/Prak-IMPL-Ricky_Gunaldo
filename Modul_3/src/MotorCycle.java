70d1611a88dd766ca4b8fa5598128a4e855ce79c
ikoks <rickygunaldo111@gmail.com>
2025-10-29 21:35:28 +0700

    Sesudah menggunakan Interface Segregation Principle (ISP), menambah file VehicleInterface, DoorInterface, Motoycyle, dan Car.

---
+++ Modul_3/src/MotorCycle.java
@@ -0,0 +1,5 @@
+public class Motorcyle : VehicleInterface {
+    override fun drive() {}
+    override fun stop() {}
+    override fun refuel() {}
+}
---
+++ Modul_3/src/VehicleInterface.java
@@ -0,0 +1,5 @@
+public interface VehicleInterface {
+    void drive();
+    void stop();
+    void refuel();
+}
