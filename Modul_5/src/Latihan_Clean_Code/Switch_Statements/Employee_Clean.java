6715706479a13a8ea73adbcfc017890c4e36b5ce
ikoks <rickygunaldo111@gmail.com>
2025-11-29 22:11:26 +0700

    Update: file Customer.java dan Employee_Clean.java

--- Modul_5/src/Latihan_Clean_Code/Switch_Statements/Employee_Clean.java
+++ Modul_5/src/Latihan_Clean_Code/Switch_Statements/Employee_Clean.java
@@ -1,15 +1,28 @@
 package Latihan_Clean_Code.Switch_Statements;
 
 public class Employee_Clean {
-    class EmployeeType {
+    public int getMonthlySalary() {
+        return 1;
+    }
+
+    public int getCommission() {
+        return 1;
+    }
+
+    public int getBonus() {
+        return 1;
+    }
+
+    abstract class EmployeeType {
         abstract int payAmount(Employee emp);
     }
-    class Salesman {
+    public class Salesman {
         int payAmount(Employee emp) {
             return emp.getMonthlySalary() + emp.getCommission();
         }
     }
-    class Manager {
+
+    public class Manager {
         int payAmount(Employee emp) {
             return emp.getMonthlySalary() + emp.getBonus();
         }
--- Modul_5/src/Latihan_Clean_Code/Use_Pronounceable_Names/Customer.java
+++ Modul_5/src/Latihan_Clean_Code/Use_Pronounceable_Names/Customer.java
@@ -3,7 +3,7 @@ package Latihan_Clean_Code.Use_Pronounceable_Names;
 import java.util.Date;
 
 public class Customer {
-    private Date enerationTimestamp;
+    private Date getModificationTimestamp;
     private Date modificationTimestamp;
     private final String recordId = "102";
     /**
