897d0b6e47a904acfbf633eda4528ff54fd33dbb
ikoks <rickygunaldo111@gmail.com>
2025-11-29 22:25:38 +0700

    Update: Memperbaharui file Point.java

--- Modul_5/src/Latihan_Clean_Code/Data_Abstraction/Point.java
+++ Modul_5/src/Latihan_Clean_Code/Data_Abstraction/Point.java
@@ -1,6 +1,10 @@
 package Latihan_Clean_Code.Data_Abstraction;
 
-public class Point {
-    public double x;
-    public double y;
+public interface Point {
+    double getX();
+    double getY();
+    void setCartesian(double x, double y);
+    double getR();
+    double getTheta();
+    void setPolar(double r, double theta);
 }
