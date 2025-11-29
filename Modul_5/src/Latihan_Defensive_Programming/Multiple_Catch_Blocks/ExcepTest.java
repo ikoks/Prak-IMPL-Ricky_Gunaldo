e1136a23fe579ebabe86f60ec04ee9284c28f7ae
ikoks <rickygunaldo111@gmail.com>
2025-11-29 22:49:51 +0700

    Update: Memperbaharui file ExcepTest.java

--- Modul_5/src/Latihan_Defensive_Programming/Multiple_Catch_Blocks/ExcepTest.java
+++ Modul_5/src/Latihan_Defensive_Programming/Multiple_Catch_Blocks/ExcepTest.java
@@ -1,5 +1,6 @@
 package Latihan_Defensive_Programming.Multiple_Catch_Blocks;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,14 +8,21 @@ import java.io.IOException;
 public class ExcepTest {
     public static void main(String args[]) {
         try {
-            file = new FileInputStream(fileName);
-            x = (byte) file.read();
-        } catch (IOException i) {
+            int a[] = new int[2];
+            System.out.println("Access element three :" + a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown :" + e);
+        }
+        System.out.println("Out of the block");
+
+        try {
+            File fileName = new File("Text.txt");
+            FileInputStream file = new FileInputStream(fileName);
+            byte x = (byte) file.read();
+        } catch (FileNotFoundException  i) {
             i.printStackTrace();
-            return -1;
-        } catch (FileNotFoundException f) {
+        } catch (IOException f) {
             f.printStackTrace();
-            return -1;
         }
     }
 }
